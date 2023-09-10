8d546c83c1d9a1379e60856c3601f4cfcbcedeb9
Wesley <wesley.brazil@outlook.com>
2023-09-10 15:53:40 -0300

    feat: logica da imagem do avatar

--- src/pages/Profile/index.tsx
+++ src/pages/Profile/index.tsx
@@ -1,6 +1,7 @@
-import React, {useState, useContext} from 'react';
+import React, {useState, useEffect, useContext} from 'react';
 import {View, Text, Modal, Platform} from 'react-native';
 import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
 import {AuthContext} from '../../contexts/auth';
 import {
   Container,
@@ -17,12 +18,24 @@ import {
 } from './styles';
 import Feather from 'react-native-vector-icons/Feather';
 import Header from '../../components/Header';
+import {launchImageLibrary} from 'react-native-image-picker';
 
 export default function Profile() {
   const {signOut, user, setUser, storageUser} = useContext(AuthContext);
   const [url, setUrl] = useState('');
   const [nome, setNome] = useState(user.name);
   const [open, setOpen] = useState(false);
+
+  useEffect(() => {
+    (async function avatarUrl() {
+      try {
+        const response = await storage().ref('users').child(user.uid).getDownloadURL();
+        setUrl(response);
+      } catch (error) {
+        console.log('Foto do usuário não encontrada');
+      }
+    })();
+  }, [])
 
   async function logOut() {
     await signOut();
@@ -42,7 +55,9 @@ export default function Profile() {
       .where('userId', '==', user.uid)
       .get();
 
-    if (postDocs.empty) return;
+    if (postDocs.empty) {
+      return;
+    }
 
     postDocs.docs.forEach(async doc => {
       await firestore().collection('posts').doc(doc.id).update({
@@ -61,16 +76,66 @@ export default function Profile() {
     setOpen(false);
   }
 
+  async function uploadFile() {
+    const options = {
+      noData: true,
+      mediaType: 'photo',
+    };
+
+    launchImageLibrary(options, response => {
+      if (response.didCancel) {
+        console.log('Cancelou');
+      } else if (response.error) {
+        console.log('Erro ao salvar a imagem');
+      } else {
+        uploadFileFirebase(response).then(() => {
+          uploadAvatarPosts();
+        });
+
+        setUrl(response.assets[0].uri);
+      }
+    });
+  }
+
+  function getFileLocalPath(response) {
+    return response.assets[0].uri;
+  }
+
+  async function uploadFileFirebase(response) {
+    const fileSource = getFileLocalPath(response);
+    const storageRef = storage().ref('users').child(user.uid);
+    return await storageRef.putFile(fileSource);
+  }
+
+  async function uploadAvatarPosts() {
+    const storageRef = storage().ref('users').child(user.uid);
+    const url = await storageRef
+      .getDownloadURL()
+      .then(async image => {
+        const postDocs = await firestore()
+          .collection('posts')
+          .where('userId', '==', user.uid)
+          .get();
+
+        postDocs.docs.forEach(async doc => {
+          await firestore().collection('posts').doc(doc.id).update({
+            avatarUrl: image,
+          });
+        });
+      })
+      .catch(error => console.log('Erro ao salvar a foto', error));
+  }
+
   return (
     <Container>
       <Header />
       {url ? (
-        <UploadButton>
+        <UploadButton onPress={() => uploadFile()}>
           <UploadText>+</UploadText>
           <Avatar source={{uri: url}} />
         </UploadButton>
       ) : (
-        <UploadButton>
+        <UploadButton onPress={() => uploadFile()}>
           <UploadText>+</UploadText>
         </UploadButton>
       )}
